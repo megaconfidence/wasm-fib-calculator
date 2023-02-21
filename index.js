3535497beae73bca93e6e589002f5c6bfbc2d039
megaconfidence <cokoghenun@gmail.com>
2023-02-21 21:58:33 +0100

    refactor: Decouples UI

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ function fib(n) {
     return fib(n - 1) + fib(n - 2);
 }
 
-document.querySelector('.fib-js-form').addEventListener('submit', (e) => {
+const form = document.querySelector('.fib-js-form');
+const input = document.querySelector('.fib-js-input');
+const display = document.querySelector('.fib-js-display');
+
+form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const num = Number(document.querySelector('.fib-js-input').value)
+    const num = Number(input.value)
     const start = performance.now();
     const val = fib(num);
     const end = performance.now();
-    document.querySelector('.fib-js-display').innerText = `${val} - in ${Math.round(end - start)}ms`;
+    display.innerText = `${val} - in ${Math.round(end - start)}ms`;
 });
--- wasm.js
+++ wasm.js
@@ -1,12 +1,16 @@
 import init, { wasmfib } from "./pkg/factorial_app_wasm.js"
 
 init().then(() => {
-    document.querySelector('.fib-wasm-form').addEventListener('submit', (e) => {
+    const form = document.querySelector('.fib-wasm-form');
+    const input = document.querySelector('.fib-wasm-input');
+    const display = document.querySelector('.fib-wasm-display');
+
+    form.addEventListener('submit', (e) => {
         e.preventDefault()
-        const num = Number(document.querySelector('.fib-wasm-input').value)
+        const num = Number(input.value)
         const start = performance.now();
         const val = wasmfib(num);
         const end = performance.now();
-        document.querySelector('.fib-wasm-display').innerText = `${val} - in ${Math.round(end - start)}ms`;
+        display.innerText = `${val} - in ${Math.round(end - start)}ms`;
     });
 })
