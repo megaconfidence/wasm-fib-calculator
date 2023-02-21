11e716faaf3525c1a3f7fbbcc66c4fc321046e10
megaconfidence <cokoghenun@gmail.com>
2023-02-21 19:39:08 +0100

    feat: Adds fib in wasm code

---
+++ wasm.js
@@ -0,0 +1,12 @@
+import init, { wasmfib } from "./pkg/factorial_app_wasm.js"
+
+init().then(() => {
+    document.querySelector('.fib-wasm-form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        const num = Number(document.querySelector('.fib-wasm-input').value)
+        const start = performance.now();
+        const val = wasmfib(num);
+        const end = performance.now();
+        document.querySelector('.fib-wasm-display').innerText = `${val} - in ${end - start}ms`;
+    });
+})
